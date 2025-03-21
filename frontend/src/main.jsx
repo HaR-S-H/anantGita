77cebd02f31df8d479a953f02695cd5f74eb69c2
HaR-S-H <119717186+HaR-S-H@users.noreply.github.com>
2025-03-21 16:32:07 +0530

    code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import AppProvider from './context/AppProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-          <GoogleOAuthProvider clientId="1079622815497-n2i10n1cu8j50umufkmj64g39i0v1rch.apps.googleusercontent.com">
+          <GoogleOAuthProvider clientId="">
     <AppProvider>
       <App />
       </AppProvider>
